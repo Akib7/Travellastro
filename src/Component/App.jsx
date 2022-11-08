3c1719aa2c119a204a787698a5009171696cb7f7
Akib Ahmed <akibahmed7360@gmail.com>
2022-11-09 00:33:53 +0600

    getting started

--- src/Component/App.jsx
+++ src/Component/App.jsx
@@ -1,13 +1,14 @@
 import React, { useRef } from "react";
 import { CssBaseline } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
+import { NavBar } from ".";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="root">
       <CssBaseline />
-      {/* <NavBar /> */}
+      <NavBar />
       <main className="content">
         <div className="toolbar" />
         my app
---
+++ src/Component/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { AppBar } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import "./navbar.styles.scss";
+
+const NavBar = () => {
+  const logo =
+    "https://fontmeme.com/permalink/221108/dce6c1a90373d92c835ccf5163407ad0.png";
+  return (
+    <div className="navbar">
+      <Link className="logo-container" to="/">
+        <img className="logo" src={logo} alt="Travellastro" />
+      </Link>
+    </div>
+  );
+};
+
+export default NavBar;
