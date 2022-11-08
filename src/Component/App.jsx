2b882260f625ca0ee718a80dc6c5004e063322ae
Akib Ahmed <akibahmed7360@gmail.com>
2022-11-08 22:49:46 +0600

    getting started

---
+++ src/Component/App.jsx
@@ -0,0 +1,20 @@
+import React, { useRef } from "react";
+import { CssBaseline } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="root">
+      <CssBaseline />
+      {/* <NavBar /> */}
+      <main className="content">
+        <div className="toolbar" />
+        my app
+        {/* <Routes><Route path="/" exact element={<}></Route></Routes> */}
+      </main>
+    </div>
+  );
+};
+
+export default App;
