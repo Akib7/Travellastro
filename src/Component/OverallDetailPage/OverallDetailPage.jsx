a7271b563f3fed58157a95c0481971f9ab101bce
Akib Ahmed <akibahmed7360@gmail.com>
2022-11-23 15:49:10 +0600

    fixing issues

--- src/Component/OverallDetailPage/OverallDetailPage.jsx
+++ src/Component/OverallDetailPage/OverallDetailPage.jsx
@@ -5,6 +5,12 @@ import PLACE_LIST from "../PlaceList/placeList.data";
 import { DetailPage } from "..";
 
 const OverallDetailPage = () => {
+  const pathName = window.location.pathname;
+  const id = parseInt(pathName.slice(8, 9));
+  console.log(id === PLACE_LIST[0].id);
+  console.log(pathName);
+  console.log(id);
+  const name = PLACE_LIST.map((name) => console.log(name.name));
   return (
     <div className="contents">{<DetailPage name={PLACE_LIST[0].name} />}</div>
   );
