d2afe2b0aaf524c36e534fdcfacb53925a561b0a
Akib Ahmed <akibahmed7360@gmail.com>
2022-12-08 21:29:49 +0600

    adding image detail page

--- src/Component/ShowAllPcture/ShowAllPicture.jsx
+++ src/Component/ShowAllPcture/ShowAllPicture.jsx
@@ -1,7 +1,29 @@
+import { Button, Grid } from "@mui/material";
 import React from "react";
+import PLACE_LIST from "./placeList.data";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import "./ShowAllPicture.styles.scss";
 
 const ShowAllPicture = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Button className="back-btn">
+        <ArrowBackIosIcon />
+      </Button>
+
+      <Grid container className="image-container">
+        <Grid item className="images">
+          {PLACE_LIST.map((swiper) =>
+            swiper.swiperImages.map((image) => (
+              <div>
+                <img src={image.image} alt={image.alt} />
+              </div>
+            ))
+          )}
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default ShowAllPicture;
